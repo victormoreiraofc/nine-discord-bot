8522cc9a39277daa6e64619d8580e1ecab1dd370
Victor Moreira <victormoreiraoficial@gmail.com>
2024-02-28 11:56:50 -0300

    Version Alpha -- Fix Ping

--- commands/ping.js
+++ commands/ping.js
@@ -9,20 +9,12 @@ module.exports = {
 
         let ping = client.ws.ping;
     
-        let embed_1 = new Discord.EmbedBuilder()
-        .setAuthor({ name: client.user.username, iconURL: client.user.displayAvatarURL({ dynamic: true }) })
-        .setDescription(`Olá ${interaction.user}, meu ping é de \`calculando...\`.`)
+        let embed = new Discord.EmbedBuilder()
+        .setAuthor({ name: interaction.user.username, iconURL: interaction.user.displayAvatarURL({ dynamic: true }) })
+        .setDescription(`Meu ping atual é de ${ping}ms.`)
         .setColor("#2B2D31");
     
-        let embed_2 = new Discord.EmbedBuilder()
-        .setAuthor({ name: client.user.username, iconURL: client.user.displayAvatarURL({ dynamic: true }) })
-        .setDescription(`Olá ${interaction.user}, meu ping é de \`${ping}ms\`.`)
-        .setColor("#2B2D31");
-    
-        interaction.reply({ embeds: [embed_1] }).then( () => {
-            setTimeout( () => {
-                interaction.editReply({ embeds: [embed_2] })
-            }, 2000)
+        interaction.reply({ embeds: [embed] }).then( () => {
         })
       }
     }
