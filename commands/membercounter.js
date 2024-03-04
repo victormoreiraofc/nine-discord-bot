b160b05ffc8578c97963fe9a506f43e80ca4141b
Mateus Ebenezer da Silva Santos <143097497+Mateus-Ebenezer@users.noreply.github.com>
2024-03-04 19:44:17 -0300

    Contador de Membros
    Ainda presente o seguinte erro na execução: 
    
    Error creating channel: db.set is not a function
    C:\Users\mateu\Documents\my_stuff\idk\nine-discord-bot-main\node_modules\@discordjs\rest\dist\index.js:722
          throw new DiscordAPIError(data, "code" in data ? data.code : data.error, status, method, url, requestData);
                ^
    
    DiscordAPIError[10062]: Unknown interaction
        at handleErrors (C:\Users\mateu\Documents\my_stuff\idk\nine-discord-bot-main\node_modules\@discordjs\rest\dist\index.js:722:13)
        at process.processTicksAndRejections (node:internal/process/task_queues:95:5)
        at async BurstHandler.runRequest (C:\Users\mateu\Documents\my_stuff\idk\nine-discord-bot-main\node_modules\@discordjs\rest\dist\index.js:826:23)
        at async _REST.request (C:\Users\mateu\Documents\my_stuff\idk\nine-discord-bot-main\node_modules\@discordjs\rest\dist\index.js:1266:22)
        at async ChatInputCommandInteraction.reply (C:\Users\mateu\Documents\my_stuff\idk\nine-discord-bot-main\node_modules\discord.js\src\structures\interfaces\InteractionResponses.js:111:5) {
      requestBody: {
        files: [],
        json: {
          type: 4,
          data: {
            content: 'O sistema foi ativado em: <#1214341334593314896>.',
            tts: false,
            nonce: undefined,
            embeds: undefined,
            components: undefined,
            username: undefined,
            avatar_url: undefined,
            allowed_mentions: undefined,
            flags: undefined,
            message_reference: undefined,
            attachments: undefined,
            sticker_ids: undefined,
            thread_name: undefined
          }
        }
      },
      rawError: { message: 'Unknown interaction', code: 10062 },
      code: 10062,
      status: 404,
      method: 'POST',
      url: 'https://discord.com/api/v10/interactions/1214341333486149693/aW50ZXJhY3Rpb246MTIxNDM0MTMzMzQ4NjE0OTY5MzoxeFhCb3htOEtaU2tZRFUyU0xYOVUxNnAzTHhyRzdyY1RaWUFnWTJVMXo0MEJ4ZDc2RkR1WHM0d0k0WmZOWGk0M2thbEVtTHVMcURZV2IyTDN6SnBzSGYyTWdiRE5nQVRkN0RoTFJ4ZEFCRUkwbENNeVBnZ3ZPUGNpT1pxUGpJZQ/callback'
    }

---
+++ commands/membercounter.js
@@ -0,0 +1,31 @@
+const Discord = require("discord.js");
+const db = require("quick.db");
+
+module.exports = {
+    name: "contador-membros",
+    description: "Mostra a quantidade de membros no servidor.",
+    type: 1,
+
+    run: async (client, interaction, args) => {
+        if (!interaction.member.permissions.has("MANAGE_GUILD")) {
+            interaction.reply({ content: "Você não tem permissão para usar este comando!", ephemeral: true });
+        } else {
+            try {
+                let channel = await interaction.guild.channels.create({
+                    name: `🗣️‼️ Membros: ${interaction.guild.memberCount}`,
+                    type: Discord.ChannelType.GuildVoice,
+                    permissionOverwrites: [{
+                        id: interaction.guild.id,
+                        deny: [Discord.PermissionFlagsBits.Connect]
+                    }]
+                });
+
+                interaction.reply(`O sistema foi ativado em: ${channel}.`);
+                db.set(`contador_${interaction.guild.id}`, channel.id);
+            } catch (error) {
+                console.error("Error creating channel:", error.message);
+                interaction.reply({ content: "Ocorreu um erro ao criar o canal.", ephemeral: true });
+            }
+        }
+    },
+};
